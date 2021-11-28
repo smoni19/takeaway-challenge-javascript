45762651b9fbc86510b5e3cf6b8686998fbc3a14
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-11-28 20:45:22 +0000

    Added show_order_summary()

--- order.js
+++ order.js
@@ -4,12 +4,18 @@ class Order {
   constructor(menu = new Menu) {
     this.menu = menu;
     this.order = [];
+    this.order_summary = "";
     this.total = 0;
   }
 
   add_item(item, amount = 1) {
     this.total += this.menu.items[item] * amount;
     this.order.push(`${amount}x ${item} ordered (£${this.menu.items[item] * amount})`);
+  }
+
+  show_order_summary()
+  {
+    return this.order_summary = `${this.order.toString()}\nTotal: £${this.total}`;
   }
 }
 
--- order.test.js
+++ order.test.js
@@ -9,4 +9,12 @@ describe('Order class', () => {
       expect(order.order).toEqual(["2x Egg fried rice ordered (£4)", "1x Beef chow mein ordered (£3.5)"]);
     });
   });
+
+  describe('show_order_summary()', () => {
+    it('should return order summary (amount of item with price) and total price', () => {
+      const order = new Order();
+      order.add_item('Egg fried rice', 2);
+      expect(order.show_order_summary()).toEqual("2x Egg fried rice ordered (£4)\nTotal: £4");
+    });
+  });
 });
